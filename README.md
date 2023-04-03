9f7ba3c990630aaef7e68ceb35c24f775356b9f0
formicant <formicant@gmail.com>
2023-04-03 21:58:51 +0700

    Auto switch off by timer

--- README.md
+++ README.md
@@ -5,35 +5,48 @@ A program for `ATTiny2313` to control the temperature of a soldering iron.
 
 ## Operation
 
+### PWM level control
+
 The controller uses PWM to control the soldering iron's temperature.
 
 Both rising and falling edges of the pulses must be synchronized with the moments when the 50 Hz AC power voltage crosses zero.
 This is possible thanks to a special circuit generating a sync pulse each time the power voltage crosses zero (falling edge).
 
-The PWM level is controlled by either of the two 10-positional switches (switch1 and switch2). A two-positional switch (switch0) defines which of the two is currently used. This allows quick switching between two temperature modes (active and passive).
+The PWM level is controlled by either of the two 10-positional switches (switch1 and switch2). A two-positional switch (switch3) defines which of the two is currently used. This allows quick switching between two temperature modes (passive (sw1) and active (sw2)).
 
-Each 10-positionsl switch has 5 pins: a ground pin and 4 binary bits (1, 2, 4, 8) representing the switch's position (0 to 9) in binary.
+Each 10-position switch has 5 pins: a ground pin and 4 binary bits (1, 2, 4, 8) representing the switch's position (0 to 9) in binary.
 
-The frequency of the PWM is 50 Hz (power) × 2 (zero crossings per period) / 9 (switch positions) ≈ 11 Hz.
+The frequency of the PWM is 50 Hz (power) × 2 (zero crossings per period) / 9 (switch positions) ≈ 11.1 Hz.
+
+
+### Timing
+
+If the active mode is switched on for `off_time2` (1 min), the controller automatically switches to the passive mode.
+
+The red active mode LED starts flashing  after `warn_time1` (5 s before mode switching).
+
+If the passive mode is switched on for `off_time`` (5 min), the controller automatically switches off the output.
+
+The green passive mode LED starts flashing  after `warn_time2` (130 s before switching off).
 
 
 ## Pins
 
-The controller uses Port B for switches 1 and 2, D6 for switch 0, T0 external timer pin (D5) for sync pulses, and OC0B pin (D4) for PWM output.
+The controller uses Port B for switches 1 and 2, D6 for switch 3, T0 external timer pin (D5) for sync pulses, and OC0B pin (D4) for PWM output.
 
 ```
                    ATTiny 2313
                    ┌───┐ ┌───┐
        (RESET) PA2─┤1  ╰─╯ 20├─VCC
-               PD0─┤2      19├─PB7 <- switch2.8
-               PD1─┤3      18├─PB6 <- switch2.4
-               PA1─┤4      17├─PB5 <- switch2.2
-               PA0─┤5      16├─PB4 <- switch2.1
-               PD2─┤6      15├─PB3 <- switch1.8
-               PD3─┤7      14├─PB2 <- switch1.4
-sync ->   (T0) PD4─┤8      13├─PB1 <- switch1.2
-pwm  <- (OC0B) PD5─┤9      12├─PB0 <- switch1.1
-switch gnd ->  GND─┤10     11├─PD6 <- switch0
+       LED1 <- PD0─┤2      19├─PB7 <- switch1.8
+       LED2 <- PD1─┤3      18├─PB6 <- switch1.4
+               PA1─┤4      17├─PB5 <- switch1.2
+               PA0─┤5      16├─PB4 <- switch1.1
+               PD2─┤6      15├─PB3 <- switch2.8
+               PD3─┤7      14├─PB2 <- switch2.4
+sync ->   (T0) PD4─┤8      13├─PB1 <- switch2.2
+pwm  <- (OC0B) PD5─┤9      12├─PB0 <- switch2.1
+switch gnd ->  GND─┤10     11├─PD6 <- switch3
                    └─────────┘
 ```
 
@@ -89,5 +102,5 @@ avrdude -c usbasp-clone -p t2313 -B 125kHz -U flash:w:soldering-iron.hex:a
 In VSCode, building and flashing can be done via tasks (`Ctrl+Shift+B`).
 
 **Warining!**
-Switch 2 uses the pins the USBasp programmer is connected to.
-Switch 2 must be in `0` position during flashing!
+Switch 1 uses the pins the USBasp programmer is connected to.
+Switch 1 must be in `0` position during flashing!
--- soldering-iron.asm
+++ soldering-iron.asm
@@ -3,19 +3,56 @@
 ; Port pins
 .equ pin_pwm  = PD5     ; output, PWM channel 0B
 .equ pin_sync = PD4     ; input, timer source T0
-.equ pin_sw0  = PD6     ; input with a pull-up resistor
+.equ pin_sw3  = PD6     ; input with a pull-up resistor, switch 3
+.equ pin_led1 = PD0     ; output, green LED indicating passive mode (1)
+.equ pin_led2 = PD1     ; output, red LED indicating active mode (2)
+
+.equ portD_output = (1<<pin_pwm) | (1<<pin_led1) | (1<<pin_led2)
+.equ portD_pullup = (1<<pin_sw3)
+
 
 ; Constants
+
 .equ max_level  = 9
+
+; time intervals for mode 1 (passive)
+.equ warn_time1 = 3000  ; * 90 ms = 4.5 min
+.equ off_time1  = 3333  ; * 90 ms = 5 min
+; time intervals for mode 2 (active)
+.equ warn_time2 =  611  ; * 90 ms = 55 s
+.equ off_time2  =  667  ; * 90 ms = 1 min
+; timeL bit used for LED flashing
+.equ flash_bit  = 2 ; flash period = 90 ms * 2^(1 + flash_bit) = 0.72 s
+
 ; timer0 PWM mode: external clock T0 (falling edge), use OCR0A as TOP
 .equ pwm_mode_a = (1<<WGM01) | (1<<WGM00)
 .equ pwm_mode_b = (1<<WGM02) | (1<<CS02) | (1<<CS01)
 .equ pwm_on     = (1<<COM0B1) ; channel B fast PWM
 
+
 ; Register aliases
 .def zero  = r0
 .def tmp   = r16
-.def level = r17
+.def mode  = r17  ; 0 = off, 1 = passive, 2 = active
+.def level = r18  ; 0..9
+.def leds  = r19
+.def sw3   = r20  ; 0 = active, 1 = passive
+.def macro_tmp = r23
+.def timeL = r24
+.def timeH = r25
+
+
+; Macros
+
+; Compares a 16-bit register pair (L:H) with an immediate value.
+.macro cpiw
+	.message "no parameters specified"
+.endm
+.macro cpiw_16_i
+      ldi   macro_tmp, high(@2)
+      cpi   @0, low(@2)
+      cpc   @1, macro_tmp
+.endm
 
 
 ; Interrupt vector table
@@ -52,12 +89,12 @@ reset:
       out   SPL, tmp
       
       ; initialize port D:
-      ;   pin_pwm  for output
+      ;   pin_pwm, pin_led1, pin_led2 for output
       ;   pin_sync for input
-      ;   pin_sw0  for input with a pull-up resistor
-      ldi   tmp, (1<<pin_pwm)
+      ;   pin_sw3  for input with a pull-up resistor
+      ldi   tmp, portD_output
       out   DDRD, tmp
-      ldi   tmp, (1<<pin_sw0)
+      ldi   tmp, portD_pullup
       out   PORTD, tmp
       
       ; initialize port B for input with pull-up resistors
@@ -73,33 +110,125 @@ reset:
       ldi   tmp, pwm_mode_b
       out   TCCR0B, tmp
       
-      ; initialize interrrupt by timer 0A
-      ldi   tmp, (1<<OCIE0A)
-      out   TIMSK, tmp
-      sei
+      ; initialize registers
+      ldi   sw3, 1            ; passive
+      ldi   mode, 1           ; passive
+      clr   timeL
+      clr   timeH
       
       ; enable idle sleep mode
       ldi   tmp, (1<<SE)
       out   MCUCR, tmp
+      
+      ; initialize interrrupt by timer 0A
+      ldi   tmp, (1<<OCIE0A)
+      out   TIMSK, tmp
+      sei
 
 loop:
       sleep
       rjmp  loop
 
 
-; Timer0 interrupt (every PWM period)
+; Timer0 interrupt (every PWM period, 90 ms)
 timer:
-      in    level, PINB       ; get switch 1 and 2 position
-      com   level             ; invert the input bits
-      sbic  PIND, pin_sw0     ; if switch0 is 1 (off)
-      swap  level             ;   then use switch 2
-      andi  level, 0x0F       ;   else use switch 1
-      subi  level, 1          ; PWM value written to OCR0B should be 
-      out   OCR0B, level      ;   less by 1 than the actual level
+      in    tmp, PIND
+      andi  tmp, (1<<pin_sw3) ; get switch 3 position
+      cp    tmp, sw3          ; compare with previous position
+      breq  same_sw3_position
+
+new_sw3_position:
+      mov   sw3, tmp
+      ; set mode
+      ldi   mode, 1           ; if sw3 == 1
+      sbrs  sw3, pin_sw3      ;   then mode = 1
+      ldi   mode, 2           ;   else mode = 2
+      ; reset time
+      clr   timeL
+      clr   timeH
+
+same_sw3_position:
+      rcall process_mode
       
+      ; set PWM value
+      dec   level             ; PWM value written to OCR0B should be 
+      out   OCR0B, level      ;   less by 1 than the actual level
       ldi   tmp, pwm_mode_a
       sbrs  level, 7          ; if the value is not -1 (actual level > 0)
       ori   tmp, pwm_on       ;   then turn on the PWM
       out   TCCR0A, tmp       ;   else turn off
       
+      ; set LEDS
+      ldi   tmp, portD_pullup
+      or    tmp, leds
+      out   PORTD, tmp
+      
       reti
+
+; Calculates `level` and `leds` by `mode` and switch position.
+; Increments `time` if `mode` != off. Changes `mode` if time is up.
+;   input:  `mode`, `time`
+;   output: `mode`, `time`, `level`, `leds`
+process_mode:
+      clr   leds
+      tst   mode
+      breq  mode0
+      
+      adiw  timeL, 1          ; increment time
+      in    level, PINB       ; get switch 1 and 2 positions
+      com   level             ; invert the input bits
+      
+      cpi   mode, 1
+      breq  mode1
+
+; Active mode
+mode2:
+      andi  level, 0x0F       ; use switch 2 value as level
+      
+      cpiw  [timeL:timeH, off_time2]
+      brsh  mode2_off_time
+      
+      cpiw  [timeL:timeH, warn_time2]
+      brlo  mode2_normal_time
+
+mode2_warn_time:
+      sbrc  timeL, flash_bit
+mode2_normal_time:
+      ldi   leds, (1<<pin_led2)
+      ret
+
+mode2_off_time:
+      ldi   mode, 1
+      clr   timeL
+      clr   timeH
+      ret
+
+
+; Passive mode
+mode1:
+      swap  level
+      andi  level, 0x0F       ; use switch 1 value as level
+      
+      cpiw  [timeL:timeH, off_time1]
+      brsh  mode1_off_time
+      
+      cpiw  [timeL:timeH, warn_time1]
+      brlo  mode1_normal_time
+
+mode1_warn_time:
+      sbrc  timeL, flash_bit
+mode1_normal_time:
+      ldi   leds, (1<<pin_led1)
+      ret
+
+mode1_off_time:
+      clr   mode
+      clr   timeL
+      clr   timeH
+      ret
+
+
+; Off mode
+mode0:
+      clr   level             ; switch off PWM
+      ret
